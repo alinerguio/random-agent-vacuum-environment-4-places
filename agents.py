d8eb1c4b875e274316014aa645c3b02bd42add4b
alinerguio <alinerguio@gmail.com>
2020-11-02 16:53:13 -0300

    Upload archives

---
+++ agents.py
@@ -0,0 +1,239 @@
+"""
+Implement Agents and Environments. (Chapters 1-2)
+The class hierarchies are as follows:
+Thing ## A physical object that can exist in an environment
+    Agent
+
+Environment ## An environment holds objects, runs simulations
+    TrivialVacuumEnvironment
+
+"""
+import random
+import collections
+import numbers
+
+
+# ______________________________________________________________________________
+
+
+class Agent():
+    """ An agent program that needs a model of the world (and of the agent itself) 
+    will have to build and maintain its own model. """
+
+    def __init__(self, program=None):
+        self.alive = True
+        self.bump = False
+        self.holding = []
+        self.performance = 0
+        if program is None or not isinstance(program, collections.abc.Callable):
+            print("Can't find a valid program for {}, falling back to default.".format(self.__class__.__name__))
+
+            def program(percept):
+                return eval(input('Percept={}; action? '.format(percept)))
+
+        self.program = program
+
+    def can_grab(self, thing):
+        """Return True if this agent can grab this thing.
+        Override for appropriate subclasses of Agent and Thing."""
+        return False
+
+
+def TraceAgent(agent):
+    """Wrap the agent's program to print its input and output. This will let
+    you see what the agent is doing in the environment."""
+    old_program = agent.program
+
+    def new_program(percept):
+        action = old_program(percept)
+        print('{} perceives {} and does {}'.format(agent, percept, action))
+        return action
+
+    agent.program = new_program
+    return agent
+
+
+# ______________________________________________________________________________
+
+
+def RandomAgentProgram(actions):
+    """An agent that chooses an action at random, ignoring all percepts.
+     list = ['Right', 'Left', 'Up', 'Down', 'Suck', 'NoOp']
+     program = RandomAgentProgram(list)
+     agent = Agent(program)
+     environment = TrivialVacuumEnvironment()
+     environment.add_thing(agent)
+     environment.run()
+     environment.status == {(1, 0): 'Clean' , (0, 0): 'Clean'}
+    True
+    """
+    return lambda percept: random.choice(actions)
+
+# ______________________________________________________________________________
+
+
+loc_A, loc_B, loc_C, loc_D = (0, 0), (1, 0), (0, 1), (1, 1)  # The two locations for the Vacuum world
+
+
+def RandomVacuumAgent():
+    """Randomly choose one of the actions from the vacuum environment.
+      agent = RandomVacuumAgent()
+      environment = TrivialVacuumEnvironment()
+      environment.add_thing(agent)
+      environment.run()
+      environment.status == {(1,0):'Clean' , (0,0) : 'Clean'}
+    True
+    """
+    return Agent(RandomAgentProgram(['Right', 'Left', 'Up', 'Down', 'Suck', 'NoOp']))
+
+# ______________________________________________________________________________
+
+
+class Environment:
+    """Abstract class representing an Environment. 'Real' Environment classes
+    inherit from this. Your Environment will typically need to implement:
+        percept:           Define the percept that an agent sees.
+        execute_action:    Define the effects of executing an action.
+                           Also update the agent.performance slot.
+    The environment keeps a list of .things and .agents (which is a subset
+    of .things). Each agent has a .performance slot, initialized to 0.
+    Each thing has a .location slot, even though some environments may not
+    need this."""
+
+    def __init__(self):
+        self.things = []
+        self.agents = []
+
+    def thing_classes(self):
+        return []  # List of classes that can go into environment
+
+    def percept(self, agent):
+        """Return the percept that the agent sees at this point. (Implement this.)"""
+        raise NotImplementedError
+
+    def execute_action(self, agent, action):
+        """Change the world to reflect this action. (Implement this.)"""
+        raise NotImplementedError
+
+    def default_location(self, thing):
+        """Default location to place a new thing with unspecified location."""
+        return None
+
+    def exogenous_change(self):
+        """If there is spontaneous change in the world, override this."""
+        pass
+
+    def is_done(self):
+        """By default, we're done when we can't find a live agent."""
+        return not any(agent.is_alive() for agent in self.agents)
+
+    def step(self):
+        """Run the environment for one time step. If the
+        actions and exogenous changes are independent, this method will
+        do. If there are interactions between them, you'll need to
+        override this method."""
+        if not self.is_done():
+            actions = []
+            for agent in self.agents:
+                if agent.alive:
+                    actions.append(agent.program(self.percept(agent)))
+                else:
+                    actions.append("")
+            for (agent, action) in zip(self.agents, actions):
+                self.execute_action(agent, action)
+            self.exogenous_change()
+
+    def run(self, steps=1000):
+        """Run the Environment for given number of time steps."""
+        for step in range(steps):
+            if self.is_done():
+                return
+            self.step()
+
+    def add_thing(self, thing, location=None):
+        """Add a thing to the environment, setting its location. For
+        convenience, if thing is an agent program we make a new agent
+        for it. (Shouldn't need to override this.)"""
+        if not isinstance(thing, Thing):
+            thing = Agent(thing)
+        if thing in self.things:
+            print("Can't add the same thing twice")
+        else:
+            thing.location = location if location is not None else self.default_location(thing)
+            self.things.append(thing)
+            if isinstance(thing, Agent):
+                thing.performance = 0
+                self.agents.append(thing)
+
+    def delete_thing(self, thing):
+        """Remove a thing from the environment."""
+        try:
+            self.things.remove(thing)
+        except ValueError as e:
+            print(e)
+            print("  in Environment delete_thing")
+            print("  Thing to be removed: {} at {}".format(thing, thing.location))
+            print("  from list: {}".format([(thing, thing.location) for thing in self.things]))
+        if thing in self.agents:
+            self.agents.remove(thing)
+
+# ______________________________________________________________________________
+# Vacuum environment
+
+
+class TrivialVacuumEnvironment(Environment):
+
+    """This environment has four locations, A, B, C and D. Each can be Dirty
+    or Clean. The agent perceives its location and the location's
+    status. This serves as an example of how to implement a simple
+    Environment."""
+
+    def __init__(self):
+        super().__init__()
+        self.status = {loc_A: random.choice(['Clean', 'Dirty']),
+                       loc_B: random.choice(['Clean', 'Dirty']),
+                       loc_C: random.choice(['Clean', 'Dirty']),
+                       loc_D: random.choice(['Clean', 'Dirty'])}
+
+    def thing_classes(self):
+        return RandomVacuumAgent
+
+    def percept(self, agent):
+        """Returns the agent's location, and the location status (Dirty/Clean)."""
+        return (agent.location, self.status[agent.location])
+
+    def execute_action(self, agent, action):
+        """Change agent's location and/or location's status; track performance.
+        Score 10 for each dirt cleaned; -1 for each move."""
+        if action == 'Right':
+            if agent.location == loc_A:
+                agent.location = loc_B
+            elif agent.location == loc_C:
+                agent.location = loc_D
+            agent.performance -= 1
+        elif action == 'Left':
+            if agent.location == loc_B:
+                agent.location = loc_A
+            elif agent.location == loc_D:
+                agent.location = loc_C
+            agent.performance -= 1
+        elif action == 'Up':
+            if agent.location == loc_C:
+                agent.location = loc_A
+            elif agent.location == loc_D:
+                agent.location = loc_B
+            agent.performance -= 1
+        elif action == 'Down':
+            if agent.location == loc_A:
+                agent.location = loc_C
+            elif agent.location == loc_B:
+                agent.location = loc_D
+            agent.performance -= 1
+        elif action == 'Suck':
+            if self.status[agent.location] == 'Dirty':
+                agent.performance += 10
+            self.status[agent.location] = 'Clean'
+
+    def default_location(self, thing):
+        """Agents start in either location at random."""
+        return random.choice([loc_A, loc_B, loc_C, loc_D])
